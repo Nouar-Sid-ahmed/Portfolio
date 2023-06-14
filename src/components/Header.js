ac703073212706fd785c3aca1dbb2c36c78abac5
Sid Ahmed Nouar <sid-ahmed.nouar@evermaps.com>
2023-06-14 18:39:35 +0200

    header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import LanguageSwitcher from './LanguageSwitcher';
 
 function Header() {
     return (
-        <header>
-            <h1>Votre nom</h1>
-            <nav>
-                <ul>
-                    <li><Link to="/">Accueil</Link></li>
-                    <li><Link to="/about">À propos de moi</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+        <header className="header">
+            <h1 className="header__title">Sid-ahmed NOUAR</h1>
+            <nav className="header__nav">
+                <ul className="header__nav-list">
+                    <li className="header__nav-item"><Link to="/">Accueil</Link></li>
+                    <li className="header__nav-item"><Link to="/about">À propos de moi</Link></li>
+                    <li className="header__nav-item"><Link to="/services">Services</Link></li>
+                    <li className="header__nav-item"><Link to="/portfolio">Portfolio</Link></li>
+                    <li className="header__nav-item"><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
             <LanguageSwitcher />
