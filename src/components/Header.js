55271594b4cbbd30aaf2a6a72172a57a32480355
Sid Ahmed Nouar <sid-ahmed.nouar@evermaps.com>
2023-06-22 22:27:03 +0200

    update readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LanguageSwitcher from './LanguageSwitcher';
+import '../styles/Header.css';
 
 function Header() {
     return (
